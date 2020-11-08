2ef8b010bc4d5af4c44d9b7cc3cc20df14895105
Grayson Cunningham <gecun289@gmail.com>
2020-11-07 19:04:38 -0700

    Add printable method and test to braille line class

--- lib/braille_line.rb
+++ lib/braille_line.rb
@@ -6,4 +6,16 @@ class BrailleLine
     @printed = line
     @braille = []
   end
+
+  def printable
+    row0 = ""
+    row1 = ""
+    row2 = ""
+    @braille.each do |char|
+      row0.concat(char[0])
+      row1.concat(char[1])
+      row2.concat(char[2])
+    end
+    row0 + "\n" + row1 + "\n" + row2
+  end
 end
--- test/braile_line_test.rb
+++ test/braile_line_test.rb
@@ -12,4 +12,14 @@ class BrailleLineTest < Minitest::Test
   def test_it_exists
     assert_instance_of BrailleLine, @line
   end
+
+  def test_printable
+    line1 = BrailleLine.new("a")
+    line2 = BrailleLine.new("ab")
+    line1.braille = [["0.", "..", ".."]]
+    line2.braille = [["0.", "..", ".."], ["0.", "0.", ".."]]
+
+    assert_equal "0.\n..\n..", line1.printable
+    assert_equal "0.0.\n..0.\n....", line2.printable
+  end
 end
