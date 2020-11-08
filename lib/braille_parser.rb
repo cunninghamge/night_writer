fd19855d05da7b42e2c5609e9e4f5a9a987f44c1
Grayson Cunningham <gecun289@gmail.com>
2020-11-08 11:13:06 -0700

    Change braille_parser's braille lines from method to instance variable

--- lib/braille_parser.rb
+++ lib/braille_parser.rb
@@ -2,20 +2,21 @@ require './lib/printed_line'
 require './lib/braille_line'
 
 class BrailleParser
-  # attr_reader :printed_lines, :braille_lines
+  attr_reader :braille_lines, :printed_lines
   #
   def initialize(braille)
-    @braille_lines = create_braille_lines(braille)
-    # @printed_lines = create_print_lines(printed)
+    @braille_lines = braille
+    @printed_lines = create_print_lines
   end
 
-  def create_braille_lines(braille)
-    separate_lines(braille).map do |line|
+  def create_print_lines
+    separate_lines.map do |line|
       PrintedLine.new(line)
     end
   end
 
-  def separate_lines(braille)
-    braille.each_slice(3).to_a
+  def separate_lines
+    @braille_lines.each_slice(3).to_a
   end
+
 end
--- test/braille_parser_test.rb
+++ test/braille_parser_test.rb
@@ -13,18 +13,27 @@ class BrailleParserTest < Minitest::Test
     assert_instance_of BrailleParser, @parser
   end
 
-  def test_it_makes_braille_lines
-    actual = @parser.create_braille_lines(@braille)
-    all_braille = actual.all? do |line|
+  def test_braille_lines
+    expected = [".0..0.00..0.0.0.0.\n", "0.........00.000.0\n", "......0.......0...\n"]
+    assert_equal expected, @parser.braille_lines
+  end
+
+  def test_it_makes_lines
+    actual = @parser.create_print_lines
+    all_printed = actual.all? do |line|
       line.class == PrintedLine
     end
 
     assert_instance_of Array, actual
-    assert_equal true, all_braille
+    assert_equal true, all_printed
   end
 
   def test_separate_lines
     expected = [[".0..0.00..0.0.0.0.\n", "0.........00.000.0\n", "......0.......0...\n"]]
-    assert_equal expected, @parser.separate_lines(@braille)
+    assert_equal expected, @parser.separate_lines
   end
+
+  # def test_it_makes_printed_lines
+  #   # assert_equal ""
+  # end
 end
