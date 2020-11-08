da1f332422a740fdda326d8eb7d8f5204b8f1294
Grayson Cunningham <gecun289@gmail.com>
2020-11-07 17:18:18 -0700

    Extract methods from PrintedLine class

--- lib/printed_line.rb
+++ lib/printed_line.rb
@@ -4,38 +4,24 @@ class PrintedLine
   def initialize(line, parent)
     @printed = line
     @parent = parent
-    @braille_lines = create_braille_lines
+    @braille_lines = split_at_max_char
   end
 
-  def split_at_max_characters
-    split_line = @printed.dup
+  def split_at_max_char
+    working_line = @printed.dup
     new_array = []
-    until split_line.strip == ""
-      line = split_line.split.reduce("") do |new_line, word|
-        new_line.concat(word, " ") if (new_line.length + word.length) <= 40
-        new_line
-      end.strip
+    until working_line.strip == ""
+      line = select_words(working_line).strip
       new_array << line
-      split_line.slice!(line)
+      working_line.slice!(line)
     end
     new_array
   end
 
-  def translate_character(character)
-    @parent.translate_character(character)
-  end
-
-  def create_braille_lines
-    split_at_max_characters.map do |string|
-      BrailleLine.new(string)
-    end
-  end
-
-  def translate_line
-    split_at_max_characters.map do |string|
-      string.chars.map do |char|
-        translate_character(char)
-      end
+  def select_words(working_line)
+    working_line.split.reduce("") do |new_line, word|
+      new_line.concat(word, " ") if (new_line.length + word.length) <= 40
+      new_line
     end
   end
 end
--- test/printed_line_test.rb
+++ test/printed_line_test.rb
@@ -20,31 +20,11 @@ class PrintedLineTest < Minitest::Test
 
   def test_split_at_max_characters
     expected = ["this sentence has more than forty", "characters."]
-    assert_equal expected, @line.split_at_max_characters
+    assert_equal expected, @line.split_at_max_char
   end
 
-  def test_translate_character
-    @parent.stubs(:translate_character).returns(["0.", "..", ".."])
-    assert_equal ["0.", "..", ".."], @line.translate_character("a")
-  end
-
-  def test_translate_line
-    line1 = PrintedLine.new("a\n", @parent)
-    @parent.stubs(:translate_character).returns(["0.", "..", ".."])
-    assert_equal [[["0.", "..", ".."]]], line1.translate_line
-
-    line2 = PrintedLine.new("aa\n", @parent)
-    @parent.stubs(:translate_character).returns(["0.", "..", ".."])
-    assert_equal [[["0.", "..", ".."], ["0.", "..", ".."]]], line2.translate_line
-  end
-
-  def test_create_braille_lines
-    actual = @line.create_braille_lines
-    assert_instance_of Array, actual
-    all_braille_lines = actual.all? do |line|
-      line.class == BrailleLine
-    end
-    assert_equal true, all_braille_lines
-    assert_equal 2, actual.count
+  def test_select_words
+    working_line = "this sentence has more than forty characters.\n"
+    assert_equal "this sentence has more than forty ", @line.select_words(working_line)
   end
 end
