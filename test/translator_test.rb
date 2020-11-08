858eb7001d350c1dc557a9bb48141845e063f2d4
Grayson Cunningham <gecun289@gmail.com>
2020-11-08 10:01:56 -0700

    Add tests for translator class to translate to print

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -4,17 +4,31 @@ require './lib/translator'
 class TranslatorTest < Minitest::Test
   include Translator
 
-  def test_translate_char
-    assert_equal ["0.", "..", ".."], translate_char("a")
-    assert_equal ["0.", "00", "0."], translate_char("r")
-    assert_equal ["00", "00", ".."], translate_char("g")
+  def test_translate_char_to_braille
+    assert_equal ["0.", "..", ".."], to_braille("a")
+    assert_equal ["0.", "00", "0."], to_braille("r")
+    assert_equal ["00", "00", ".."], to_braille("g")
   end
 
-  def test_translate_line
+  def test_translate_line_to_braille
     line1 = "a"
-    assert_equal [["0.", "..", ".."]], translate_line(line1)
+    assert_equal [["0.", "..", ".."]], translate_line(line1, :to_braille)
 
     line2 = "ab"
-    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], translate_line(line2)
+    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], translate_line(line2, :to_braille)
+  end
+
+  def test_translate_char_to_print
+    assert_equal "a", to_braille(["0.", "..", ".."])
+    assert_equal "r", to_braille(["0.", "00", "0."])
+    assert_equal "g", to_braille(["00", "00", ".."])
+  end
+
+  def test_translate_line_to_print
+    line1 = [["0.", "..", ".."]]
+    assert_equal "a", translate_line(line1, :to_print)
+
+    line2 = [["0.", "..", ".."], ["0.", "0.", ".."]]
+    assert_equal "ab", translate_line(line2, :to_print)
   end
 end
