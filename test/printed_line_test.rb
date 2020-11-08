af4150ccae079940e483b56a95159dc8a19dad4a
Grayson Cunningham <gecun289@gmail.com>
2020-11-08 08:47:16 -0700

    Add tests for PrintedLine class

--- test/printed_line_test.rb
+++ test/printed_line_test.rb
@@ -3,15 +3,24 @@ require './lib/printed_line'
 
 class PrintedLineTest < Minitest::Test
   def setup
-    text = "this sentence has more than forty characters.\n"
-    @line = PrintedLine.new(text)
+    braille = ["0.0.\n..0.\n...."]
+    @line = PrintedLine.new(braille)
   end
 
   def test_it_exists
     assert_instance_of PrintedLine, @line
   end
 
-  def test_attributes
-    assert_equal "this sentence has more than forty characters.\n", @line.text
+  def test_braille
+    expected = ["0.0.\n..0.\n...."]
+    assert_equal expected, @line.braille
+  end
+
+  def test_parse_braille_chars
+    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], @line.braille_chars
+  end
+
+  def test_translate_line
+    assert_equal "ab\n", @line.printed
   end
 end
