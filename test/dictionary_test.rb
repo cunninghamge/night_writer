cbeb1acb2ca1e88a1aa2830cefad0232e4df214c
Grayson Cunningham <gecun289@gmail.com>
2020-11-09 16:19:17 -0700

    Convert translator to dictionary

---
+++ test/dictionary_test.rb
@@ -0,0 +1,52 @@
+require_relative './test_helper'
+require './lib/dictionary'
+
+class DictionaryTest < Minitest::Test
+  include Dictionary
+
+  def test_translate_char_to_braille
+    assert_equal ["0.", "..", ".."], to_braille("a")
+    assert_equal ["0.", "00", "0."], to_braille("r")
+    assert_equal ["00", "00", ".."], to_braille("g")
+  end
+
+  def test_translate_line_to_braille
+    line1 = ["a"]
+    assert_equal [["0.", "..", ".."]], translate_line(line1, :to_braille)
+
+    line2 = ["a","b"]
+    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], translate_line(line2, :to_braille)
+  end
+
+  def test_translate_char_to_print
+    assert_equal "a", to_print(["0.", "..", ".."])
+    assert_equal "r", to_print(["0.", "00", "0."])
+    assert_equal "g", to_print(["00", "00", ".."])
+  end
+
+  def test_translate_line_to_print
+    line1 = [["0.", "..", ".."]]
+    assert_equal ["a"], translate_line(line1, :to_print)
+
+    line2 = [["0.", "..", ".."], ["0.", "0.", ".."]]
+    assert_equal ["a","b"], translate_line(line2, :to_print)
+  end
+
+  def test_punctuation
+    assert_equal "!", to_print(["..", "00", "0."])
+    assert_equal "'", to_print(["..", "..", "0."])
+    assert_equal ",", to_print(["..", "0.", ".."])
+    assert_equal "-", to_print(["..", "..", "00"])
+    assert_equal ".", to_print(["..", "00", ".0"])
+    assert_equal "?", to_print(["..", "0.", "00"])
+    assert_equal ":", to_print(["..", "00", ".."])
+
+    assert_equal ["..", "00", "0."], to_braille("!")
+    assert_equal ["..", "..", "0."], to_braille("'")
+    assert_equal ["..", "0.", ".."], to_braille(",")
+    assert_equal ["..", "..", "00"], to_braille("-")
+    assert_equal ["..", "00", ".0"], to_braille(".")
+    assert_equal ["..", "0.", "00"], to_braille("?")
+    assert_equal ["..", "00", ".."], to_braille(":")
+  end
+end
