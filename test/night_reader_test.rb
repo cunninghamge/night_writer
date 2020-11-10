54131c103734e7f9745060b3e2fcd9c8c3ebd047
Grayson Cunningham <gecun289@gmail.com>
2020-11-09 18:16:04 -0700

    Update night_reader tests for refactor

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -1,26 +1,18 @@
-# require_relative './test_helper'
-# require './lib/night_reader'
-#
-# class NightReaderTest < Minitest::Test
-#   def setup
-#     ARGV.replace(['./data/short_braille.txt', 'message.txt'])
-#     @night_reader = NightReader.new
-#   end
-#
-#   def test_it_exists
-#     assert_instance_of NightReader, @night_reader
-#   end
-#
-#   def test_it_makes_a_file_io
-#     assert_instance_of FileIO, @night_reader.file
-#   end
-#
-#   def test_it_makes_a_parser
-#     assert_instance_of BrailleParser, @night_reader.parser
-#   end
-#
-#   def test_output
-#     message = ("Created 'message.txt' containing 10 characters\n")
-#     assert_output(message) {puts @night_reader.message}
-#   end
-# end
+ARGV.replace(['./data/short_braille.txt', 'message.txt'])
+require_relative './test_helper'
+require './lib/night_reader'
+
+class NightReaderTest < Minitest::Test
+  def setup
+    @night_reader = NightReader.new
+  end
+
+  def test_it_exists
+    assert_instance_of NightReader, @night_reader
+  end
+
+  def test_output
+    message = ("Created 'message.txt' containing 10 characters\n")
+    assert_output(message) {puts @night_reader.message}
+  end
+end
