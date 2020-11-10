b3b7b94cdb1c9f6b79f597da80a65e928a274ba2
Grayson Cunningham <gecun289@gmail.com>
2020-11-09 21:10:25 -0700

    Add functionality to support caps and edge cases in PrintParser

--- lib/print_parser.rb
+++ lib/print_parser.rb
@@ -10,22 +10,45 @@ class PrintParser
   end
 
   def split_at_max_char(text)
-    new_array = []
+    lines = []
     until text.strip == ""
-      line = select_words(text).strip
-      new_array << line
+      line = select_words(text).rstrip
+      lines << line
       text.slice!(line)
     end
-    new_array
+    lines
   end
 
   def select_words(text)
-    text.split.reduce("") do |new_line, word|
-      if (new_line.length + word.length) <= 40
-        new_line.concat(word, " ")
+    split_with_spaces(text).reduce("") do |selected, word|
+      if word_length(word) > 40
+        return reduce_to_40(word)
+      elsif check_length(selected, word)
+        selected.concat(word)
       else
-        return new_line
+        return selected
       end
+    end
+  end
+
+  def split_with_spaces(text)
+    text.split(/(?= )/)
+  end
+
+  def reduce_to_40(word)
+    until word_length(word) <= 40
+      word = word[0..-2]
+    end
+    word
+  end
+
+  def check_length(selected, word)
+    (word_length(selected) + word_length(word)) <= 40
+  end
+
+  def word_length(phrase)
+    split_with_spaces(phrase).reduce(0) do |count, word|
+      count + word.length + word.count(*"A-Z") + word.count(*"0-9")
     end
   end
 end
