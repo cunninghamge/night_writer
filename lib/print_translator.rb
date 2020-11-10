a3b08891e42fbf9967c9f424a49d8d6b5d05cfa5
Grayson Cunningham <gecun289@gmail.com>
2020-11-09 19:33:08 -0700

    Create PrintTranslator class with tests

---
+++ lib/print_translator.rb
@@ -0,0 +1,39 @@
+require './lib/file_io'
+require './lib/braille_parser'
+require './lib/line'
+
+class PrintTranslator
+  attr_reader :file_io, :parser, :lines
+
+  def initialize(args)
+    @file_io = FileIO.new(args)
+    @parser = BrailleParser.new(@file_io.read)
+    @lines = create_line_objects
+    translate_lines
+    # writeable_lines = compile_lines
+    # @file_io.write_to_file(writeable_lines)
+  end
+
+  def create_line_objects
+    separate_braille_lines.map do |line|
+      Line.new(line)
+    end
+  end
+
+  def separate_braille_lines
+    @parser.separate_braille_lines
+  end
+
+  def translate_lines
+    @lines.each do |line|
+      line.translate_to_print
+    end
+  end
+
+  def compile_lines
+    compiled = @lines.reduce("") do |string, line|
+      string.concat(line.printable_text)
+    end
+    compiled
+  end
+end
--- test/print_translator_test.rb
+++ test/print_translator_test.rb
@@ -29,27 +29,29 @@ class PrintTranslatorTest < Minitest::Test
   end
 
   def test_separate_braille_lines
-    expected = []
-    assert_equal expected, @translator.separate_braille_lines
+    args = ['./data/one_char_braille.txt', 'message.txt']
+    translator = PrintTranslator.new(args)
+
+    expected = [[["0.", "..", ".."]]]
+    assert_equal expected, translator.separate_braille_lines
   end
 
   def test_translate_lines
-    args = ['./data/one_char.txt', 'braille.txt']
+    args = ['./data/one_char_braille.txt', 'braille.txt']
     translator = PrintTranslator.new(args)
 
-    expected = [[".0..0.00..0.0.0.0.\n", "0.........00.000.0\n", "......0.......0...\n"]]
-    assert_equal expected, translator.lines[0].translated_text
+    assert_equal ["a"], translator.lines[0].translated_text
   end
 
   def test_compile_lines
-    assert_equal "i am here\n", translator.compile_lines
+    assert_equal "i am here\n", @translator.compile_lines
   end
 
   def test_multiple_lines
     args = ['./data/sentence_braille.txt', 'message.txt']
-    @translator = PrintTranslator.new(args)
+    translator = PrintTranslator.new(args)
 
     expected = "this sentence has more than forty\ncharacters\nthis one does not\n"
-    assert_equal expected, parser.compile_print
+    assert_equal expected, translator.compile_lines
   end
 end
